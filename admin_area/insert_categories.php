1e387da82d6af0d79051a80f0163695ba41b2857
Mahdi-Mouldi <146773043+Mahdi-Mouldi@users.noreply.github.com>
2025-04-09 01:58:10 +0100

    insert categories into the database

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -1,11 +1,28 @@
+<?php
+    include('../include/connect.php');
+    if(isset($_POST['insert_cat'])){
+        $category_title=$_POST['cat_title'];
+        $select_query="select * from `categories` where category_title='$category_title'";
+        $result_select=mysqli_query($con,$select_query);
+        $number=mysqli_num_rows($result_select);
+        if($number>0){
+            echo "<script>alert('this category present in the database')</script>";
+        }else{
+            $insert_query="insert into `categories` (category_title) values ('$category_title')";
+            $result_query=mysqli_query($con,$insert_query);
+            if($result_query){
+                echo "<script>alert('category has been inserted successfully')</script>";
+            }
+        }
+    }
+?>
 <form action="" method="POST" class="mb-2">
     <div class="input-group w-90 mb-3">
     <span class="input-group-text bg-info" id="basic-addon1"><i id="basic-addon1" class="fa-solid fa-receipt"></i></span>
-    <input type="text" class="form-control" placeholder="insert categories" aria-label="Categories" aria-describedby="basic-addon1">
+    <input type="text" class="form-control" placeholder="insert categories" name="cat_title" aria-label="Categories" aria-describedby="basic-addon1">
     </div>
     <div class="input-group w-10 mb-2 m-auto">
-        <!-- <input type="submit" class="form-control bg-info" name="insert_cat" value="Insert Categories" > -->
-        <button class="bg-info p-3 my-3 border-0">Insert Categories</button>
+        <input type="submit" class="form-control bg-info" name="insert_cat" value="Insert Categories" >
     </div>
 
 </form>
