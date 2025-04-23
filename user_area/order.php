973551d1b97b517cd8acb43b196a0e0812d08f01
Mahdi-Mouldi <146773043+Mahdi-Mouldi@users.noreply.github.com>
2025-04-23 21:59:22 +0100

    Orders page-Inserting

--- user_area/order.php
+++ user_area/order.php
@@ -8,6 +8,8 @@
     $total_price=0;
     $cart_query_price="select * from `cart_details` where ip_adress='$get_ip'";
     $res=mysqli_query($con,$cart_query_price);
+    $invoice_number=mt_rand();
+    $status="pending";
     $count_products=mysqli_num_rows($res);
     while($row_price=mysqli_fetch_array($res)){
         $product_id=$row_price['product_id'];
@@ -18,5 +20,25 @@
             $product_values=array_sum($product_price);
             $total_price+=$product_values;
         }
+    }
+    // getting quantity from cart
+    $get_cart="select * from `cart_details`";
+    $res_cart=mysqli_query($con,$get_cart);
+    $get_item_quantity=mysqli_fetch_array($res_cart);
+    $quantity=$get_item_quantity['quantity'];
+    if($quantity==0){
+        $quantity=1;
+        $subtotal=$total_price;
+    }else{
+        $quantity=$quantity;
+        $subtotal=$total_price * $quantity;
+
+    }
+    $insert_orders="insert into `user_orders` (user_id,amount_due,invoice_number,total_products,order_date,order_status) values ('$user_id','$subtotal','$invoice_number','$count_products',NOW(),'$status')";
+    $res_insert=mysqli_query($con,$insert_orders);
+    if($res_insert){
+        echo "<script>alert('Orders are submitted succesfully')</script>";
+        echo "<script>window.open('profile.php','_self')</script>";
+
     }
 ?>
